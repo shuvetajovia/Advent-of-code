cf322fafae0ee8f446318545a9d49583104b960b
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2025-12-10 23:34:18 +0530

    Add Day10Part2solutions.py for Advent of Code
    Implement solution for Day 10 Part 2 of Advent of Code, including reading input, processing button configurations, and solving the optimization problem using Z3.

---
+++ Day10Part2solutions.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+import sys
+from functools import cache
+from collections import defaultdict, Counter, deque
+import z3
+
+# --- DIRECT FILE INPUT ---
+with open(r"E:\advent\input.txt", "r") as f:
+    D = f.read()
+
+p1 = 0
+p2 = 0
+
+for line in D.splitlines():
+    words = line.split()
+    goal = words[0]
+    goal = goal[1:-1]
+    goal_n = 0
+    for i, c in enumerate(goal):
+        if c == '#':
+            goal_n += 2**i
+
+    buttons = words[1:-1]
+    B = []
+    NS = []
+    for button in buttons:
+        ns = [int(x) for x in button[1:-1].split(',')]
+        button_n = sum(2**x for x in ns)
+        B.append(button_n)
+        NS.append(ns)
+
+    score = len(buttons)
+    for a in range(2**len(buttons)):
+        an = 0
+        a_score = 0
+        for i in range(len(buttons)):
+            if ((a >> i) % 2) == 1:
+                an ^= B[i]
+                a_score += 1
+        if an == goal_n:
+            score = min(score, a_score)
+    p1 += score
+
+    # solve Ax = B
+    joltage = words[-1]
+    joltage_ns = [int(x) for x in joltage[1:-1].split(',')]
+    V = []
+    for i in range(len(buttons)):
+        V.append(z3.Int(f'B{i}'))
+
+    EQ = []
+    for i in range(len(joltage_ns)):
+        terms = []
+        for j in range(len(buttons)):
+            if i in NS[j]:
+                terms.append(V[j])
+        eq = (sum(terms) == joltage_ns[i])
+        EQ.append(eq)
+
+    o = z3.Optimize()
+    o.minimize(sum(V))
+    for eq in EQ:
+        o.add(eq)
+    for v in V:
+        o.add(v >= 0)
+
+    assert o.check()
+    M = o.model()
+    for d in M.decls():
+        p2 += M[d].as_long()
+
+print(p1)
+print(p2)
